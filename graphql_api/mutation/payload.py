c3233eaa039ed2d84f1892398c6e11794df59450
Ibis Prevedello <ibiscp@gmail.com>
2023-01-25 14:14:35 -0300

    feat: add asynchronous option to training model

--- graphql_api/mutation/payload.py
+++ graphql_api/mutation/payload.py
@@ -10,15 +10,16 @@ class CreatePayload(graphene.Mutation):
     class Arguments:
         project_id = graphene.ID()
         information_source_id = graphene.ID()
+        asynchronous = graphene.Boolean(required=False)
 
     payload = graphene.Field(InformationSourcePayload)
 
-    def mutate(self, info, project_id: str, information_source_id: str):
+    def mutate(self, info, project_id: str, information_source_id: str, asynchronous: bool = True):
         auth.check_demo_access(info)
         auth.check_project_access(info, project_id)
         user = get_user_by_info(info)
         payload = manager.create_payload(
-            info, project_id, information_source_id, user.id
+            info, project_id, information_source_id, user.id, asynchronous
         )
         return CreatePayload(payload)
 
